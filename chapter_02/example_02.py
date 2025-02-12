6be77dd3aabd2055324d2a5f36a347491cdf7edd
Woody <mwkang19@gmail.com>
2025-02-12 21:34:13 +0900

    Add Chapter_02: Perceptron

--- chapter_02/example_02.py
+++ chapter_02/example_02.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
@@ -9,17 +10,19 @@ def NAND(x1, x2):
         return 0
     else:
         return 1
-    
+
+
 def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.2
     tmp = np.sum(w*x + b)
-    if tmp <=0:
+    if tmp <= 0:
         return 0
     else:
         return 1
-    
+
+
 print(NAND(0, 0))  # 0을 출력
 print(NAND(1, 0))  # 0을 출력
 print(NAND(0, 1))  # 0을 출력
--- chapter_02/example_03.py
+++ chapter_02/example_03.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
@@ -9,7 +10,8 @@ def AND(x1, x2):
         return 0
     elif tmp > 0:
         return 1
-    
+
+
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
@@ -19,22 +21,25 @@ def NAND(x1, x2):
         return 0
     else:
         return 1
-    
+
+
 def OR(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.2
     tmp = np.sum(w*x + b)
-    if tmp <=0:
+    if tmp <= 0:
         return 0
     else:
         return 1
+
 
 def XOR(x1, x2):
     s1 = NAND(x1, x2)
     s2 = OR(x1, x2)
     y = AND(s1, s2)
     return y
+
 
 print(XOR(0, 0))
 print(XOR(1, 0))
